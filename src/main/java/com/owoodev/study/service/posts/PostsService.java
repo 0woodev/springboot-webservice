bedb6575f1c9e56ccec0e33585581d5bb935d324
0woodev <0woodev@gmail.com>
2022-01-20 19:16:37 +0900

    도메인 모델을 이용한 Posts API 제작

--- src/main/java/com/owoodev/study/service/posts/PostsService.java
+++ src/main/java/com/owoodev/study/service/posts/PostsService.java
@@ -1,4 +1,19 @@
 package com.owoodev.study.service.posts;
 
+import com.owoodev.study.domain.posts.PostsRepository;
+import com.owoodev.study.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
