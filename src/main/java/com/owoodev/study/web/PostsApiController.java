d3ef7841205fd2f84cc75fea38a48a4559f1080a
2016025350 <2016025350@hanyang.ac.kr>
2022-01-20 19:16:37 +0900

    도메인 모델을 이용한 Posts API 제작

--- src/main/java/com/owoodev/study/web/PostsApiController.java
+++ src/main/java/com/owoodev/study/web/PostsApiController.java
@@ -1,4 +1,20 @@
 package com.owoodev.study.web;
 
+import com.owoodev.study.service.posts.PostsService;
+import com.owoodev.study.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
 }
