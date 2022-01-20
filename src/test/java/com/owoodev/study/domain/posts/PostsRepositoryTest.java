945d56d67cc49037b928c343492430a4da953d49
0woodev <0woodev@gmail.com>
2022-01-20 18:17:41 +0900

    PostsRepository 단위테스트 제작

--- src/test/java/com/owoodev/study/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/owoodev/study/domain/posts/PostsRepositoryTest.java
@@ -1,7 +1,48 @@
 package com.owoodev.study.domain.posts;
 
-import static org.junit.Assert.*;
+import javafx.beans.binding.BooleanExpression;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/***
+ * <Li> @ SpringBootTest : H2 데이터베이스를 자동으로 실행해 줍니다.</Li>
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void save_post_and_load() {
+        String title ="test post";
+        String content = "body part";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("0woodev@gmail.com")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
 
 }
