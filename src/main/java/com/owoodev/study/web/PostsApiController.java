80e4e3a009bdfc9eb6bebc3445b89eaeae370954
2016025350 <2016025350@hanyang.ac.kr>
2022-01-21 11:48:03 +0900

    게시글 수정 API 구현

--- src/main/java/com/owoodev/study/web/PostsApiController.java
+++ src/main/java/com/owoodev/study/web/PostsApiController.java
@@ -1,11 +1,10 @@
 package com.owoodev.study.web;
 
 import com.owoodev.study.service.posts.PostsService;
+import com.owoodev.study.web.dto.PostsResponseDto;
 import com.owoodev.study.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +15,10 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
