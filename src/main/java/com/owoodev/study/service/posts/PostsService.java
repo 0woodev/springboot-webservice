7f5710c61327f01bcf921851ba714677323b22c7
0woodev <0woodev@gmail.com>
2022-01-21 11:48:03 +0900

    게시글 수정 API 구현

--- src/main/java/com/owoodev/study/service/posts/PostsService.java
+++ src/main/java/com/owoodev/study/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.owoodev.study.service.posts;
 
+import com.owoodev.study.domain.posts.Posts;
 import com.owoodev.study.domain.posts.PostsRepository;
+import com.owoodev.study.web.dto.PostsResponseDto;
 import com.owoodev.study.web.dto.PostsSaveRequestDto;
+import com.owoodev.study.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +18,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
+        );
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
+        );
+        return new PostsResponseDto(entity);
     }
 }
