733da426bea9dd30e893974b98f43e55541c1ae8
0woodev <0woodev@gmail.com>
2022-01-21 23:50:18 +0900

    메인페이지에 게시글 테이블 구현

---
+++ src/main/java/com/owoodev/study/web/dto/PostsListResponseDto.java
@@ -0,0 +1,24 @@
+package com.owoodev.study.web.dto;
+
+import com.owoodev.study.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.createdDate = entity.getCreatedDate();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
