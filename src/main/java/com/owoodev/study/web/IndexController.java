2f890dcf825271853503f6e39059f9157533e7ba
Youngwoo <0woodev@gmail.com>
2022-01-23 17:23:24 +0900

    게시글수정 페이지 구현

--- src/main/java/com/owoodev/study/web/IndexController.java
+++ src/main/java/com/owoodev/study/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.owoodev.study.web;
 
 import com.owoodev.study.service.posts.PostsService;
+import com.owoodev.study.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -3,6 +3,10 @@ let main = {
         $('#btn-save').on('click', () => {
             main.save();
         });
+
+        $('#btn-update').on('click', () => {
+            main.update();
+        });
     },
     save: () => {
         let data = {
@@ -22,7 +26,28 @@ let main = {
             window.location.href='/';
         }).fail((err) => {
             alert(JSON.stringify(err));
-        })
+        });
+    },
+    update: () => {
+        let data = {
+            title: $('#title').val(),
+           content: $('#content').val(),
+        };
+
+        let id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset="utf-8"',
+            data: JSON.stringify(data)
+        }).done(() => {
+            alert('글이 수정되었습니다.');
+            window.location.href='/';
+        }).fail((err) => {
+            alert(JSON.stringify(err));
+        });
     }
 }
 
