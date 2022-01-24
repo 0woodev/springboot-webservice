506b8dcaff95d443dd891dace8a3b35aa9ab6453
0woodev <0woodev@gmail.com>
2022-01-24 17:32:46 +0900

    User 도메인 메서드 추가 1. update 메서드 2. 권한 getter 메서드

--- src/main/java/com/owoodev/study/domain/user/User.java
+++ src/main/java/com/owoodev/study/domain/user/User.java
@@ -36,4 +36,15 @@ public class User extends BaseTimeEntity {
         this.picture = picture;
         this.role = role;
     }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 }
