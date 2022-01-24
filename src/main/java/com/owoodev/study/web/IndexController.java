d6304100698b997d54ef0d3b4911dedce776b112
0woodev <0woodev@gmail.com>
2022-01-24 18:06:40 +0900

    깃 이그노어 문제로 현재까지 한 작업 한번에 커밋 1. 로그인 버튼 추가 2. OAuth 로그인 기능 추가 3. 깃 이그노어 추가

--- src/main/java/com/owoodev/study/web/IndexController.java
+++ src/main/java/com/owoodev/study/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.owoodev.study.web;
 
+import com.owoodev.study.config.auth.dto.SessionUser;
 import com.owoodev.study.service.posts.PostsService;
 import com.owoodev.study.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) model.addAttribute("userName", user.getName());
+
         return "index";
     }
 
