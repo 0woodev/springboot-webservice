51cdb262afd77ffb75b317bf21dcefc8a6a76263
0woodev <0woodev@gmail.com>
2022-01-25 20:00:29 +0900

    annotation LoginUser 구현

--- src/main/java/com/owoodev/study/web/IndexController.java
+++ src/main/java/com/owoodev/study/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.owoodev.study.web;
 
+import com.owoodev.study.config.auth.LoginUser;
 import com.owoodev.study.config.auth.dto.SessionUser;
 import com.owoodev.study.service.posts.PostsService;
 import com.owoodev.study.web.dto.PostsResponseDto;
@@ -19,10 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
